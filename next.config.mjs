493631ef05a4f972c8fc333c27a090a8209241e2
dehong <1737407069@qq.com>
2025-01-12 00:01:01 +0800

    change code

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 3600,
+  },
+  poweredByHeader: false,
+  skipMiddlewareUrlNormalize: true,
 };
 
 export default nextConfig;
